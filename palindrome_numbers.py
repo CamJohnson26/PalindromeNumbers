2d0d578a70f7de29e076bc486746c49ac52cedfd
Cameron Johnson <cameronrossjohnson@gmail.com>
2017-11-28 00:01:45 -0600

    Added unit tests and refactored classes
    Cleaned up the code and added unit tests. removed unused code constants

--- palindrome_numbers.py
+++ palindrome_numbers.py
@@ -1,74 +1,128 @@
+"""
+    Cameron Johnson
+    November 2017
+"""
+
 import math
 
-chars_readable = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-chars = list(range(0, 1000))
+class PalindromeNumbers():
 
-def convert_num_to_base(i, b):
-	if i < 0:
-		print("Error: Negative numbers not supported")
-		return ""
-	r = []
-	power = int(math.floor(math.log(i, b)))
-	processing = i
-	while power >= 0:
-		digit = 0
-		while processing - pow(b, power) >= 0:
-			processing -= pow(b, power)
-			digit += 1
-		r.append(chars[digit])
-		power -= 1
-	return r
+    @staticmethod
+    def convert_num_to_base(i, b):
+        """
+        i = number to convert
+        b = base
 
-def is_palindrome(s):
-	le = len(s)
-	if le == 1:
-		return True
-	for i, v in enumerate(s):
-		if i >= (le/2):
-			return True
-		if v != s[-i-1]:
-			break 
-	return False
+        Returns an array of integers, representing the characters
+        of the number converted to the base
+        """
+        if i < 0:
+            raise Exception("Error: Negative numbers not supported")
+        if b < 2:
+            raise Exception("Error: Bases less than 2 not supported")
+        r = []
+        power = int(math.floor(math.log(i, b)))
+        processing = i
+        while power >= 0:
+            digit = 0
+            while processing - pow(b, power) >= 0:
+                processing -= pow(b, power)
+                digit += 1
+            r.append(digit)
+            power -= 1
+        return r
 
+    @staticmethod
+    def is_palindrome(s):
+        """
+            s = string to test
 
-n = 1000
-results = [None] * n
-for b in range(2, len(chars)):		# loop through all bases
-	pending = [i + 1 for i, v in enumerate(results) if v == None]
-	found = [p for p in pending if is_palindrome(convert_num_to_base(p, b))]
-	for i, f in enumerate(found):
-		results[f - 1] = b
-	if len([r for r in results if r is not None]) == 0:
-		break
+            Returns whether the given string is a palindrome
+        """
+        le = len(s)
+        if le == 1:
+            return True
+        for i, v in enumerate(s):
+            if i >= (le/2):
+                return True
+            if v != s[-i-1]:
+                break 
+        return False
 
-for i, r in enumerate(results):
-	list_rep = (convert_num_to_base(i+1, r) if r is not None else "None")
-	string_rep = ""
-	for l in list_rep:
-		if l < len(chars_readable):
-			string_rep += chars_readable[l]
-		else:
-			string_rep += "?"
-			print(len(chars_readable))
-			print("Found one too long")
-	print("{0}, {1}".format(str(i + 1), str(r)))
-#print(len([r for r in results if r is None]))
+    @staticmethod
+    def find_palindromes(n):
+        """
+            n = numbers to check
 
-#for i in range(2, 36):
-#	print(convert_num_to_base(39, i))
+            Find the lowest base in which a range of numbers
+            is a palindrome.
+        """
+        results = [None] * n
+        for b in range(2, n):       # loop through all bases
+            pending = [i + 1 for i, v in enumerate(results) if v == None]
+            found = [p for p in pending if PalindromeNumbers.is_palindrome(PalindromeNumbers.convert_num_to_base(p, b))]
+            for i, f in enumerate(found):
+                results[f - 1] = b
+            if len([r for r in results if r is not None]) == 0:
+                break
+        return results
+
+if __name__ == "__main__":
+    for i, n in enumerate(PalindromeNumbers.find_palindromes(1000)):
+        print("{0}, {1}".format(i + 1, n))
+
 
 import unittest
 
 class TestPalindrome(unittest.TestCase):
  
-    def setUp(self):
-        pass
+    def test_convert_num_to_base(self):
+        f = PalindromeNumbers.convert_num_to_base
+
+        # i, b = r
+        # Test negative base
+        self.assertRaises(Exception, f, (2, -1))
+
+        # Test negative number
+        self.assertRaises(Exception, f, (-1, 2))
+
+        # Test 0 base
+        self.assertRaises(Exception, f, (2, 0))
+
+        # Test float
+        self.assertRaises(Exception, f, (2.2, 0))
+
+        # Test string
+        self.assertRaises(Exception, f, ("2", 0))
+
+        # Test general accuracy
+        self.assertEqual(f(2, 2), [1, 0])
+        self.assertEqual(f(2, 3), [2])
+        self.assertEqual(f(24, 2), [1, 1, 0, 0, 0])
+        self.assertEqual(f(1036, 13), [6, 1, 9])
+        self.assertEqual(f(18974, 755), [25, 99])
  
-    def test_numbers_3_4(self):
-        self.assertEqual( multiply(3,4), 12)
- 
-    def test_strings_a_3(self):
-        self.assertEqual( multiply('a',3), 'aaa')
+    def is_palindrome(self):
+        f = PalindromeNumbers.is_palindrome
+
+        # Test empty string
+        self.assertEqual("", True)
+
+        # Test duplicates
+        self.assertEqual("001", False)
+        self.assertEqual("AABBCCC", False)
+        self.assertEqual("ZZZZZZZZZ", True)
+        self.assertEqual("ZZZZZZZZ", True)
+
+        # Test case
+        self.assertEqual("AABBaa", False)
+        self.assertEqual("AABBAA", True)
+
+    def find_palindromes(self):
+        f = PalindromeNumbers.find_palindromes
+
+        # Test subset
+        self.assertEqual(f(10), [])
  
 if __name__ == '__main__':
     unittest.main()
